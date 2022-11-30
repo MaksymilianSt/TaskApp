d2b94f2493349c30af4d9300604ece5bed9f262c
MaksymilianSt <Maksymilianst02@gmail.com>
2022-11-30 20:19:52 +0100

    ProjectService created

---
+++ src/main/java/TaskApp/demo/logic/TaskGroupService.java
@@ -0,0 +1,40 @@
+package TaskApp.demo.logic;
+
+import TaskApp.demo.model.TaskGroup;
+import TaskApp.demo.model.TaskGroupRepository;
+import TaskApp.demo.model.TaskRepository;
+import TaskApp.demo.model.projection.GroupReadModel;
+import TaskApp.demo.model.projection.GroupWriteModel;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskGroupService {
+    private TaskGroupRepository repository;
+    private TaskRepository taskRepository;
+
+    public TaskGroupService(TaskGroupRepository repository, TaskRepository taskRepository) {
+        this.repository = repository;
+        this.taskRepository = taskRepository;
+    }
+
+    public GroupReadModel createGroup(GroupWriteModel source){
+        TaskGroup result = repository.save(source.toGroup());
+        return new GroupReadModel(result);
+    }
+    public List<GroupReadModel> readAll(){
+        return repository.findAll().stream()
+                .map(taskGroup ->  new GroupReadModel(taskGroup))
+                .collect(Collectors.toList());
+    }
+    public void toggleGroup(int groupId ){
+        if(taskRepository.existsByDoneIsFalseAndGroup_Id(groupId)){
+            throw new IllegalStateException("group has undone tasks . Do all the tasks first");
+        }
+       TaskGroup result = repository.findById(groupId)
+                .orElseThrow(()-> new IllegalArgumentException("Task group with given id not found"));
+        result.setDone(!result.isDone());
+    }
+}
